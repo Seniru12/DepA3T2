c1f1f774284f6343eb225e2deacfe3f9327c1d8c
Seniru12 <168343447+Seniru12@users.noreply.github.com>
2024-09-04 13:02:33 +1100

    Add files via upload

---
+++ DepA3T2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Clear(); // Clear the console screen
+            Console.WriteLine("Current Date and Time:");
+            
+            // Display the current date and time
+            Console.WriteLine(DateTime.Now.ToString("dddd, MMMM dd, yyyy hh:mm:ss tt"));
+            
+            Thread.Sleep(1000); // Pause for 1 second
+        }
+    }
+}
